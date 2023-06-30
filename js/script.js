bb4af67390d3b72dab177e622b47d960060064ba
Roman Pecherskikh <pwpec95@gmail.com>
2023-06-30 21:27:38 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+new Swiper('.price-slider', {
+    slidesPerView: 2.5,
+    simulateTouch: true,
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+    },
+    spaceBetween: 87,
+    loop: false,
+    loopedSlides: 0,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+
+const mediaQuery = window.matchMedia('(max-width: 992px)');
+
+if (mediaQuery.matches) {
+    new Swiper('.price-slider', {
+        slidesPerView: 1,
+        simulateTouch: true,
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+        },
+        spaceBetween: 87,
+        loop: false,
+        loopedSlides: 0,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    }); // Вызываем функцию, если разрешение изменилось и соответствует условию
+} else {
+  // Здесь можно добавить код для обработки, если разрешение больше или меньше 700px
+}
