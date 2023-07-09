7a3ceeb00b233b7f5d0e6693e076de58ebda9fe5
Roman Pecherskikh <pwpec95@gmail.com>
2023-07-09 12:40:54 +0300

    09.07.2023

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Слайдер swiper
 new Swiper('.price-slider', {
     slidesPerView: 2.5,
     simulateTouch: true,
@@ -9,7 +10,6 @@ new Swiper('.price-slider', {
     loop: false,
     loopedSlides: 0,
 });
-
 
 const mediaQuery = window.matchMedia('(max-width: 992px)');
 
@@ -31,4 +31,36 @@ if (mediaQuery.matches) {
     }); // Вызываем функцию, если разрешение изменилось и соответствует условию
 } else {
   // Здесь можно добавить код для обработки, если разрешение больше или меньше 700px
+}
+
+//Всплывающий header при scroll
+window.onscroll = function showHeader () {
+
+    let header = document.querySelector('.header');
+
+    if(window.pageYOffset > 80) {
+        header.classList.add('header__fixed')
+    } else{
+        header.classList.remove('header__fixed')
+    }
+}
+
+//Прокрутка при клике
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if(menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
 }
