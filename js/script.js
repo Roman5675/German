0fac24abc70d96ce1b911f51f9e570f05180f5da
Roman Pecherskikh <pwpec95@gmail.com>
2023-07-09 19:25:34 +0300

    09.07.2023a

--- js/script.js
+++ js/script.js
@@ -45,7 +45,18 @@ window.onscroll = function showHeader () {
     }
 }
 
-//Прокрутка при клике
+//Меню бургер
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
+
+//Прокрутка при клике на меню header
 const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
 if(menuLinks.length > 0) {
     menuLinks.forEach(menuLink => {
@@ -56,6 +67,13 @@ if(menuLinks.length > 0) {
         if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
             const gotoBlock = document.querySelector(menuLink.dataset.goto);
             const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+            
+            if(iconMenu.classList.contains('_active')) {
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
+
             window.scrollTo({
                 top: gotoBlockValue,
                 behavior: "smooth"
